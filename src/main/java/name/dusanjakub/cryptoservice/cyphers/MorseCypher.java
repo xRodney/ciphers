5c61a3de089a3a18f77ed52e9d82de156264bc4c
Dusan Jakub <dusan.jakub@gmail.com>
2019-07-12 21:44:28 +0200

    Morse Cypher

---
+++ src/main/java/name/dusanjakub/cryptoservice/cyphers/MorseCypher.java
@@ -0,0 +1,81 @@
+package name.dusanjakub.cryptoservice.cyphers;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.util.Map.Entry;
+import static java.util.stream.Collectors.toMap;
+
+/**
+ * Implements the Morse code cyphers
+ */
+@Service("morse")
+public class MorseCypher implements Cypher {
+    private static final Map<Character, String> toMorse = new HashMap<>();
+    private static final Map<String, String> fromMorse;
+
+    static {
+        toMorse.put('A', ".-");
+        toMorse.put('B', "-...");
+        toMorse.put('C', "-.-.");
+        toMorse.put('D', "-..");
+        toMorse.put('E', ".");
+        toMorse.put('F', "..-.");
+        toMorse.put('G', "--.");
+        toMorse.put('H', "....");
+        toMorse.put('I', "..");
+        toMorse.put('J', ".---");
+        toMorse.put('K', "-.-");
+        toMorse.put('L', ".-..");
+        toMorse.put('M', "--");
+        toMorse.put('N', "-.");
+        toMorse.put('O', "---");
+        toMorse.put('P', ".--.");
+        toMorse.put('Q', "--.-");
+        toMorse.put('R', ".-.");
+        toMorse.put('S', "...");
+        toMorse.put('T', "-");
+        toMorse.put('U', "..-");
+        toMorse.put('V', "...-");
+        toMorse.put('W', ".--");
+        toMorse.put('X', "-..-");
+        toMorse.put('Y', "-.--");
+        toMorse.put('Z', "--..");
+
+        toMorse.put('1', ".----");
+        toMorse.put('2', "..---");
+        toMorse.put('3', "...--");
+        toMorse.put('4', "....-");
+        toMorse.put('5', ".....");
+        toMorse.put('6', "-....");
+        toMorse.put('7', "--...");
+        toMorse.put('8', "---..");
+        toMorse.put('9', "----.");
+        toMorse.put('0', "-----");
+
+        toMorse.put(' ', "");
+
+        fromMorse = toMorse.entrySet().stream().collect(toMap(Entry::getValue, v -> v.getKey().toString()));
+    }
+
+    @Override
+    public String encrypt(String text) {
+        return text.chars()
+                .mapToObj(c -> toMorse.get(Character.toUpperCase((char) c)))
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(" "));
+    }
+
+    @Override
+    public String decrypt(String text) {
+        return Pattern.compile("[^.-]")
+                .splitAsStream(text)
+                .map(x -> fromMorse.getOrDefault(x, x))
+                .collect(Collectors.joining());
+    }
+}
---
+++ src/test/java/name/dusanjakub/cryptoservice/cyphers/MorseCypherTest.java
@@ -0,0 +1,49 @@
+package name.dusanjakub.cryptoservice.cyphers;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MorseCypherTest {
+
+    private MorseCypher cypher = new MorseCypher();
+
+    @Test
+    public void encrypt() {
+        assertEquals("String 'bang' is encrypted",
+                "-... .- -. --.", cypher.encrypt("bang"));
+        assertEquals("Lower case alphabet",
+                ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..",
+                cypher.encrypt("abcdefghijklmnopqrstuvwxyz"));
+        assertEquals("Upper case alphabet",
+                ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..",
+                cypher.encrypt("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        assertEquals("Numbers",
+                "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.",
+                cypher.encrypt("0123456789"));
+        assertEquals("Invalid chars are skipped, case is irrelevant",
+                "-... .- -. --. -... .- -. --.",
+                cypher.encrypt("BaNg!!!Bang"));
+        assertEquals("Spaces are kept",
+                "--- -. .  - .-- ---  - .... .-. . .",
+                cypher.encrypt("One, two, three"));
+    }
+
+    @Test
+    public void decrypt() {
+        assertEquals("String 'bang' is encrypted",
+                "BANG", cypher.decrypt("-... .- -. --."));
+        assertEquals("Upper case alphabet",
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ", cypher.decrypt(".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.."));
+        assertEquals("Numbers",
+                "0123456789", cypher.decrypt("----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----."));
+        assertEquals("Invalid chars are skipped, case is always upper",
+                "BANGBANG", cypher.decrypt("-... .- -. --. -... .- -. --."));
+        assertEquals("Spaces are kept",
+                "ONE TWO THREE", cypher.decrypt("--- -. .  - .-- ---  - .... .-. . ."));
+        assertEquals("Invalid chars are delimiters",
+                "ONE T", cypher.decrypt("---k-.x.rr-"));
+        assertEquals("Illegal morse sequences are passed through", "ON.---.----", cypher.decrypt("--- -. .---.----"));
+    }
+}
